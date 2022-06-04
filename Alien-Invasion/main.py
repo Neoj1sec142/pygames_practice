34d1a4909eabc43f2e72b29cf8819d44b0392cd9
Mark Harmon <neothej1sec@gmail.com>
2022-06-03 22:53:50 -0500

    set up more game functions

--- Alien-Invasion/main.py
+++ Alien-Invasion/main.py
@@ -2,18 +2,22 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+import game_functions as gf
 
 def run_game():
     pygame.init()
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
-    ship = Ship(screen)
+    ship = Ship(ai_settings, screen)
     # set background color
     bg_color = (230, 230, 230)
     #main game loop:
     while True:
         # watch for keyboard mouse events
+        gf.check_events(ship)
+        ship.update()
+        gf.update_screen(ai_settings, screen, ship)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
