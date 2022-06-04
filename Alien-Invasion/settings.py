1d34574ef7497691e39f1e1f94d073d67ed17b96
Mark Harmon <neothej1sec@gmail.com>
2022-06-03 22:22:47 -0500

    set up settings

---
+++ Alien-Invasion/settings.py
@@ -0,0 +1,8 @@
+# A class to store all settings for Alien Invasion
+class Settings():
+    def __init__(self):
+        #initialize game settings
+        #screen seetings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
