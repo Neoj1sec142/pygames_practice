8bec31f5a45cbcaf8b2a9bb0c2647f64b0e57894
Mark Harmon <neothej1sec@gmail.com>
2022-06-04 11:16:15 -0500

    added bullet class

--- Alien-Invasion/ship.py
+++ Alien-Invasion/ship.py
@@ -5,6 +5,7 @@ class Ship():
         self.screen = screen
         self.ai_settings = ai_settings
         self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
